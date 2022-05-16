598c3b48a3b0acf14cfdc230885aabf7a986f385
Michael Chung <mikechung89@gmail.com>
2022-05-15 22:07:01 -0400

    backend template

--- backend/controllers/itineraryController.js
+++ backend/controllers/itineraryController.js
@@ -1,38 +1,38 @@
 // This is for interacting with mongoDB
 // because we get a promise when interacting with Mongo
-const asyncHandler = require('express-async-handler')
+// const asyncHandler = require('express-async-handler')
 // @desc        Get User Itinerary
 // @route       GET /api/itinerary
 // @access      Private
-const getItinerary = asyncHandler(async (req, res) => {
+const getItinerary = async (req, res) => {
     res.status(200).json({message: "Getting itinerary"})
-})
+}
 
 // @desc        Set User Itinerary
 // @route       POST /api/itinerary
 // @access      Private     
-const postItinerary = asyncHandler(async (req, res) => {
-    if (!req.body.text) {
+const postItinerary = async (req, res) => {
+    if (!req.body) {
         res.status(400)
         throw new Error('Please add something')
     }
-    console.log(req.body)
-    res.status(200).json({message: "Posting new itinerary"})
-})
+    console.log(req)
+    res.status(200).json({message: `Posting ${req.body.title} to new itinerary`})
+}
 
 // @desc        Update User Itinerary
 // @route       PATCH /api/itinerary/:id
 // @access      Private
-const updateItinerary = asyncHandler(async (req, res) => {
+const updateItinerary = async (req, res) => {
     res.status(200).json({message: `Updating itinerary at ID:${req.params.id}`})
-})
+}
 
 // @desc        Delete Itinerary
 // @route       DELETE /api/itinerary/:id
 // @access      Private
-const deleteItinerary = asyncHandler(async (req, res) => {
+const deleteItinerary = async (req, res) => {
     res.status(200).json({message: `Deleting itinerary ID:${req.params.id}`})
-})
+}
 
 module.exports = {
     getItinerary,
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
-console.log("Hello World")
+const express = require('express')
+const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
+const port = 3000
+
+
+const app = express()
+
+app.use(express.json())
+app.use(express.json())
+
+app.use('/api/itinerary', require('./routes/itineraryRoute'))
+app.use(errorHandler)
+app.listen(port, () => console.log(`Server started on port: ${port}`))
