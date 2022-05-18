24af342e85189fef399cd61346e0bb3b76a9db1b
Michael Chung <mikechung89@gmail.com>
2022-05-18 18:57:11 -0400

    style login page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,45 +1,55 @@
-import { useState } from 'react'
-import Box from '@mui/material/Box'
-import Button from '@mui/material/Button'
-import TextField from '@mui/material/TextField'
-import Typography from '@mui/material/Typography'
+import { useState } from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
 
-function Login () {
-    const defaultState = {
-        email: "",
-        password: ""
-    }
-    const [formData, setFormData] = useState(defaultState)
+function Login() {
+  const defaultState = {
+    email: "",
+    password: "",
+  };
+  const [formData, setFormData] = useState(defaultState);
 
-    function handleSubmit () {
-        fetch('/loginuser', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                Accept: 'application/json'
-            },
-            body: JSON.stringify(formData)})
-            .then(res => {
-                if (res.ok){
-                    const headers = res.headers.get('auth-token')
-                    localStorage.setItem('user',headers)}
-            })
-        }
-            
+  function handleSubmit() {
+    fetch("/loginuser", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(formData),
+    }).then((res) => {
+      if (res.ok) {
+        const headers = res.headers.get("auth-token");
+        localStorage.setItem("user", headers);
+      }
+    });
+  }
 
+  function handleChange(e) {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  }
 
-    function handleChange (e) {
-        setFormData({...formData, [e.target.name]: e.target.value,})
-    }
-
-    return (
-        <Box component="form" >
-            <Typography>Login Here</Typography>
-            <TextField name='email' value={formData.email} onChange={handleChange}/>
-            <TextField name='password' value={formData.password} onChange={handleChange}/>
-            <Button onClick={handleSubmit}>Submit</Button>
+  return (
+      <Paper>
+        <Box alignItems sx={{margin: 'auto'}} component="form">
+          <Typography>Login Here</Typography>
+          <TextField
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+          <TextField
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+          <Button onClick={handleSubmit}>Submit</Button>
         </Box>
-    )
+      </Paper>
+  );
 }
 
-export default Login
+export default Login;
