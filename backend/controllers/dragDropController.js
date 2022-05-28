d2ee3794a52df3527ebf3bc3755537c8744dfa8a
Michael Chung <mikechung89@gmail.com>
2022-05-28 00:43:03 -0400

    add error handling to backend

--- backend/controllers/dragDropController.js
+++ backend/controllers/dragDropController.js
@@ -2,13 +2,21 @@ const Itinerary = require('../model/Itinerary')
 
 const updateOneDate = async (req, res) => {
     const updateItinerary = await Itinerary.findByIdAndUpdate(req.body.id, {places: req.body.reordered},{returnOriginal: false})
-    res.status(200).send(updateItinerary)
+    try {
+        res.status(200).send(updateItinerary)
+    } catch(err) {
+        res.status(400).send(err)
+    }
 }
 
 const updateTwoDates = async (req, res) => {
     const updateFirst = await Itinerary.findByIdAndUpdate(req.body.firstDate.id,{places: req.body.firstDate.reordered},{returnOriginal: false})
     const updateSecond = await Itinerary.findByIdAndUpdate(req.body.secondDate.id,{places: req.body.secondDate.reordered},{returnOriginal: false})
-    res.status(200).send([updateFirst,updateSecond])
+    try {
+        res.status(200).send([updateFirst,updateSecond])
+    } catch (err) {
+        res.status(400).send(err)
+    }
 }
 
 module.exports = {
--- backend/controllers/tripController.js
+++ backend/controllers/tripController.js
@@ -2,52 +2,59 @@ const Trip = require('../model/Trip')
 const Itinerary = require('../model/Itinerary')
 const User = require('../model/User')
 
-const showTrip = async (req,res) => {
-    if(req.params._id === 'null') {
-      return res.status(400).send({errors: "No tripId sent"})
-    }
+const showTrip = async (req, res) => {
+  if (req.params._id === 'null') {
+    return res.status(400).send({ errors: "No tripId sent" })
+  }
+  try {
     const getData = await Trip.findById(req.params._id)
-    const itineraryData = await Itinerary.find({_id: { $in: getData.itineraries}}).sort({date: 1})
-    // const placesData = await Place.find({place_id : { $in: itineraryData.places}})
+    const itineraryData = await Itinerary.find({ _id: { $in: getData.itineraries } }).sort({ date: 1 })
     res.status(201).send([itineraryData, getData._id])
+  } catch (err) {
+    res.status(400).send(err)
+  }
 }
 
-const postTrip = async (req,res) => {
-    const newItineraryDates = async () => {
-      return Promise.all(
-        req.body.dates.map((each) => {
-          const itinerary = new Itinerary({
-            itineraryInfo: {},
-            places: [],
-            date: each,
-          });
-          const saved = itinerary.save();
-          return saved;
-        })
-      );
-    };
-    //Make new itineraries and get ids to place in new Trip
-    const newDates = await newItineraryDates();
-    const mapIds = newDates.map(each => each._id)
+const postTrip = async (req, res) => {
+  const newItineraryDates = async () => {
+    return Promise.all(
+      req.body.dates.map((each) => {
+        const itinerary = new Itinerary({
+          itineraryInfo: {},
+          places: [],
+          date: each,
+        });
+        const saved = itinerary.save();
+        return saved;
+      })
+    );
+  };
+  //Make new itineraries and get ids to place in new Trip
+  const newDates = await newItineraryDates();
+  const mapIds = newDates.map(each => each._id)
 
-    //Make a trip document and add itinerary date document references
-    const tripData = new Trip({
-        name: req.body.tripName,
-        itineraries: mapIds
-    });
-    const newTrip = await tripData.save()
+  //Make a trip document and add itinerary date document references
+  const tripData = new Trip({
+    name: req.body.tripName,
+    itineraries: mapIds
+  });
+  const newTrip = await tripData.save()
 
-    // Associate the new trip to user
+  // Associate the new trip to user
+  try {
     const user = await User.findByIdAndUpdate(
       req.body.userId,
-      {$push: {itineraries: {tripName: req.body.tripName, tripId: newTrip._id}}},
-      {returnOriginal: false}
+      { $push: { itineraries: { tripName: req.body.tripName, tripId: newTrip._id } } },
+      { returnOriginal: false }
     )
-    res.status(201).send({updatedUser: user, newTrip: newTrip})
+    res.status(201).send({ updatedUser: user, newTrip: newTrip })
+  } catch (err) {
+    res.status(400).send(err)
+  }
 }
 
 //Add dates
-const addDates = async (req,res) => {
+const addDates = async (req, res) => {
   //Need to get a list of all existing itinerary dates
   const newItineraryDates = async () => {
     return Promise.all(
@@ -66,52 +73,39 @@ const addDates = async (req,res) => {
   const mapIds = newDates.map(each => each._id)
   const updatedTrip = await Trip.findOneAndUpdate(
     { _id: req.params._id },
-    { $push: { itineraries: { $each: mapIds}}},
-    {returnOriginal: false}
+    { $push: { itineraries: { $each: mapIds } } },
+    { returnOriginal: false }
   );
-  const itineraryData = await Itinerary.find({_id: { $in: updatedTrip.itineraries}}).sort({date: 1})
-  res.status(200).send(itineraryData)
+  try {
+    const itineraryData = await Itinerary.find({ _id: { $in: updatedTrip.itineraries } }).sort({ date: 1 })
+    res.status(200).send(itineraryData)
+  } catch (err) {
+    res.status(400).send(err)
+  }
 }
 
-const trimDates = async (req,res) => {
+const trimDates = async (req, res) => {
   // Delete the itinerarie dates
   const itineraryDatesDelete = req.body.idsToDelete.map(each => each._id)
-  await Itinerary.deleteMany({_id: {$in: itineraryDatesDelete}})
+  await Itinerary.deleteMany({ _id: { $in: itineraryDatesDelete } })
   //Get the trip
   const mappedIds = req.body.dates.map(each => each._id)
   const updatedTrip = await Trip.findOneAndUpdate(
     { _id: req.params._id },
-    { itineraries: mappedIds},
-    {returnOriginal: false}
+    { itineraries: mappedIds },
+    { returnOriginal: false }
   );
-  const itineraryData = await Itinerary.find({_id: { $in: updatedTrip.itineraries}}).sort({date: 1})
-  res.status(200).send(itineraryData)
-  // const newItineraryDates = async () => {
-  //   return Promise.all(
-  //     req.body.dates.map((each) => {
-  //       const itinerary = new Itinerary({
-  //         itineraryInfo: {},
-  //         places: [],
-  //         date: each,
-  //       });
-  //       const saved = itinerary.save();
-  //       return saved;
-  //     })
-  //   );
-  // };
-  // const newDates = await newItineraryDates();
-  // const mapIds = newDates.map(each => each._id)
-  // const updatedTrip = await Trip.findOneAndUpdate(
-  //   { _id: req.params._id },
-  //   { $push: { itineraries: { $each: mapIds}}}
-  // );
-  // const itineraryData = await Itinerary.find({_id: { $in: updatedTrip.itineraries}}).sort({date: 1})
-  // res.status(200).send(itineraryData)
+  try {
+    const itineraryData = await Itinerary.find({ _id: { $in: updatedTrip.itineraries } }).sort({ date: 1 })
+    res.status(200).send(itineraryData)
+  } catch (err) {
+    res.status(400).send(err)
+  }
 }
 
 module.exports = {
-    showTrip,
-    postTrip,
-    addDates,
-    trimDates
+  showTrip,
+  postTrip,
+  addDates,
+  trimDates
 }
