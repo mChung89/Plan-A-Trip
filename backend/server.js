72e8231a4d297cea576ef5a81eba07fd1df08af9
Michael Chung <mikechung89@gmail.com>
2022-06-22 21:41:41 -0400

    changed path

--- backend/server.js
+++ backend/server.js
@@ -23,10 +23,10 @@ app.use(cookieParser());
 const path = require("path");
 
 // Step 1:
-app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.use(express.static(path.resolve(__dirname, "../client/build")));
 // Step 2:
 app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+  response.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
 
 // Routes
